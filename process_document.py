2710964287c184bfc361d6c3eefccdc53a7f663e
Brian Ong <brianong@Brians-MacBook-Pro.local>
2021-09-08 20:54:15 +1000

    Add phrase tokenisation by noun and verb phrase, write tokens to output files

--- process_document.py
+++ process_document.py
@@ -6,12 +6,17 @@ This script takes a PDF file and performs text preprocessing to transform the do
 removal, lemmatization, case normalisation, parts of speech (POS) tagging, phrase chunking to extract noun and verb
 phrases. The output will be a plain text file.
 containing all extracted phrase tokens.
+
+TODO: replace named entities with xxx in phrase tokens
+    case normalisation of word and phrase tokens
+    replace numbers with 0 instead of nothing??? maybe
 """
 
 from nltk.tokenize import sent_tokenize
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from nltk.stem import WordNetLemmatizer
+from nltk import Tree
 import nltk.tag
 import sys
 import logging
@@ -56,43 +61,73 @@ def main():
     # set up nlp preprocessing
     stopWords = set(stopwords.words("english"))
     lemmatizer = WordNetLemmatizer()
-    chunkGrammar = r"""
-      NP: {<DT|JJ|NN.*>+}          # Chunk sequences of DT, JJ, NN
-      VP: {<VB.*><NP|PP|CLAUSE>+$} # Chunk verbs and their arguments
-      """
-    chunkPhraseParser = nltk.RegexpParser(chunkGrammar)
+    chunkGrammarNp = """NP: {<DT>?<JJ>*<NN>}    #Noun Phrases """
+    chunkGrammarVp1 = """VP: {<VB.*><DT>?<JJ>*<NN><RB.?>?}   #Verb Phrase type 1"""
+    chunkGrammarVp2 = """VP: {<DT>?<JJ>*<NN><VB.*><RB.?>?}   #Verb Phrase type 2"""
+    chunkPhraseParserNp = nltk.RegexpParser(chunkGrammarNp)
+    chunkPhraseParserVp1 = nltk.RegexpParser(chunkGrammarVp1)
+    chunkPhraseParserVp2 = nltk.RegexpParser(chunkGrammarVp2)
 
     for artefact in artefactFiles:
-        with open(artefact, 'rb') as f:
-            text = extract_text(f)
-            # remove punctuation
-            # credit to: https://stackoverflow.com/questions/265960/best-way-to-strip-punctuation-from-a-string
-            text = re.sub(r'[^\w\s]', '', text)
-            # remove numbers
-            text = re.sub(r'[0-9]', '', text)
-            # remove underscores
-            text = text.replace('_', '')
+        with open(artefact, 'rb') as inFile:
+            fileName = artefact[:-4].split('/')[1]
 
-            wordTokensRaw = word_tokenize(text)
+            if artefact[-4:] == '.pdf':
+                text = extract_text(inFile)
+            elif artefact[-4:] == '.txt':
+                text = inFile.read()
+            else:
+                logger.error('Input file type not recognised, ensure files are of type .pdf or .txt')
+                exit(1)
 
-            wordTokens = [word for word in wordTokensRaw if word not in stopWords]
-            wordTokens = [lemmatizer.lemmatize(word) for word in wordTokens]
+        # remove punctuation
+        # credit to: https://stackoverflow.com/questions/265960/best-way-to-strip-punctuation-from-a-string
+        text = re.sub(r'[^\w\s]', '', text)
+        # remove numbers
+        text = re.sub(r'[0-9]', '', text)
+        # remove underscores
+        text = text.replace('_', '')
 
-            # named entity removal
-            # credit to: https://stackoverflow.com/questions/43742956/fast-named-entity-removal-with-nltk
-            namedEntitiesTree = nltk.ne_chunk(nltk.pos_tag(wordTokens))
-            wordTokens = [leaf[0] for leaf in namedEntitiesTree if type(leaf) != nltk.Tree]
-            print(wordTokens)
+        wordTokensRaw = word_tokenize(text)
+        wordTokensLemmatized = [lemmatizer.lemmatize(word) for word in wordTokensRaw]
+        wordTokens = [word for word in wordTokensLemmatized if word not in stopWords]
 
-            # # create chunk tree
-            # phraseChunkTree = nltk.RegexpParser(chunkGrammar).parse(nltk.pos_tag(wordTokens))
-            #
-            # phraseChunkTree.draw()
-            #
-            # # extract noun and verb phrases
-            # wordTokens = [leaf[0] for leaf in phraseChunkTree if type(leaf) != nltk.Tree]
-            #
-            # print(wordTokens)
+        # named entity removal
+        # credit to: https://stackoverflow.com/questions/43742956/fast-named-entity-removal-with-nltk
+        namedEntitiesTree = nltk.ne_chunk(nltk.pos_tag(wordTokens))
+        wordTokens = [leaf[0] for leaf in namedEntitiesTree if type(leaf) != nltk.Tree]
+
+        # create chunk tree
+        phraseChunkTreeNp = chunkPhraseParserNp.parse(nltk.pos_tag(wordTokensRaw))
+        phraseChunkTreeVp1 = chunkPhraseParserVp1.parse(nltk.pos_tag(wordTokensRaw))
+        phraseChunkTreeVp2 = chunkPhraseParserVp2.parse(nltk.pos_tag(wordTokensRaw))
+
+        # extract text for noun and verb phrases and join together all phrases into a single array
+        phraseTokens = extractChunkText(phraseChunkTreeNp, 'NP') + extractChunkText(phraseChunkTreeVp1, 'VP') + extractChunkText(phraseChunkTreeVp2, 'VP')
+
+        with open('DTMs/' + fileName + '-phrases.txt', 'w') as outFile:
+            for phrase in phraseTokens:
+                outFile.write(phrase)
+                outFile.write('\n')
+
+        with open('DTMs/' + fileName + '-words.txt', 'w') as outFile:
+            for word in wordTokens:
+                outFile.write(word)
+                outFile.write('\n')
+
+
+def extractChunkText(chunkTree, chunkType):
+    # get the leaves of the all noun phrase subtrees into arrays
+    # credit to https://stackoverflow.com/questions/52021855/nltk-linguistic-tree-traversal-and-extract-noun-phrase-np
+    chunkLeaves = [subtree.leaves() for subtree in chunkTree if type(subtree) == Tree and subtree.label() == chunkType]
+
+    # discard the POS tag for each word, join the words of each phrase array into a string
+    for i in range(len(chunkLeaves)):
+        for j in range(len(chunkLeaves[i])):
+            chunkLeaves[i][j] = chunkLeaves[i][j][0]
+        chunkLeaves[i] = " ".join(chunkLeaves[i])
+
+    return chunkLeaves
 
 
 if __name__ == '__main__':
